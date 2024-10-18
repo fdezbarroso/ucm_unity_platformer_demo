857acdc050581111f6bd6f64c28f7270800a4f70
fdezbarroso <f.fdezbarroso@gmail.com>
2024-10-18 21:02:24 +0200

    Implement simple UI, keys, and green level

--- Assets/Scripts/TriggerLoadAdditive.cs
+++ Assets/Scripts/TriggerLoadAdditive.cs
@@ -3,26 +3,39 @@ using UnityEngine;
 public class TriggerLoadAdditive : MonoBehaviour
 {
     // TODO 1 - Añadir variable pública con el color de la puerta (tipo GameManager.DoorColor)
-
+    [SerializeField]
+    private GameManager.DoorColor m_color = GameManager.DoorColor.GREEN;
     /// <summary>
     /// La comprobación del tipo de gameobject que entra en el trigger se hace por tag
     /// El valor del tag que nos interesa se guarda en esta variable
     /// </summary>
-    private string m_PlayerTag = "Player";
+    private const string m_PlayerTag = "Player";
+    private GameManager m_GameManager = null;
 
-	/// <summary>
-	/// Detecta cuándo un GameObject entra en el trigger al cual está asignado este componente.
-	/// En nuestro caso, realizamos la carga aditiva del nivel indicado en el atributo
-	/// público "LevelToLoadName"
-	/// </summary>
-	void OnTriggerEnter(Collider other)
-	{
-		//LoadLevelAdditive(other.gameObject);
-	}
+    private void Start()
+    {
+        m_GameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    }
+
+    /// <summary>
+    /// Detecta cuándo un GameObject entra en el trigger al cual está asignado este componente.
+    /// En nuestro caso, realizamos la carga aditiva del nivel indicado en el atributo
+    /// público "LevelToLoadName"
+    /// </summary>
+    void OnTriggerEnter(Collider other)
+    {
+        LoadLevelAdditive(other.gameObject);
+    }
 
 
     // TODO 2 - Hacer función que cargue un nivel de forma aditiva.
     // Será necesario pasarle el gameObject que ha colisionado con el trigger
     // Tras cargar el nivel, desactivamos el trigger
-    
+    private void LoadLevelAdditive(GameObject go)
+    {
+        if (m_GameManager != null)
+        {
+            m_GameManager.TriggerLoadAdditive(m_color);
+        }
+    }
 }
